2f66d3dc528c39b4b935fd361f85e8866a211f99
adityaraut1256-sudo <adityaraut1256@gmail.com>
2026-01-06 21:02:12 +0530

    first commit

---
+++ Engine_room.py
@@ -0,0 +1,23 @@
+# Let's store a string (string = any kind of text) in a variable. In the following example, 'dummy' is a variable which holds some text data (string) 
+# Use print() to print something (it's not necessary in google colab, but tag along with us here for now)
+
+dummy = "Hello world!"
+print(dummy)# Let's explore other data type quickly before focusing our attention to strings
+
+num = 7
+another_num = 4.0
+print(type(num))
+print(type(another_num))
+
+num1 = 9
+num2 = 11
+print(num1, num2)
+print(type(num1), type(num2))
+
+# Let's accept input from user
+# input is a special function that allows user to type (and pass) user's own input
+
+name = input('raju')   # once this is run, type anything in the box
+print(name)
+print(type(name))
+
